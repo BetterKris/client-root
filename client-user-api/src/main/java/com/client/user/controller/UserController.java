2a797b06627c75717262764237a42b4541d3a881
200525112@qq.com <200525112@qq.com>
2021-09-01 10:37:30 +0800

    commit test

--- client-user-api/src/main/java/com/client/user/controller/UserController.java
+++ client-user-api/src/main/java/com/client/user/controller/UserController.java
@@ -31,6 +31,7 @@ public class UserController {
 
     @GetMapping("/list")
     public List<User> list(User user){
+
         PageHelper.startPage(user.getPageNum() , user.getPageSize());
         List<User> users = userService.list(user);
         List<User> resultList = new ArrayList<>();
